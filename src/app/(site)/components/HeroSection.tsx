1edebd6adb72adb8093bde6a324f828f55d5f01c
JayasuryaV007 <jayasurya@mavencart.com>
2024-11-07 16:51:43 +0530

    modify landing page

--- src/app/(site)/components/HeroSection.tsx
+++ src/app/(site)/components/HeroSection.tsx
@@ -1,7 +1,7 @@
 const HeroSection = () => {
   return (
     <div className="overflow-x-hidden bg-gray-50">
-      <section className="relative py-12 sm:py-16 lg:pt-20 xl:pb-0">
+      <section className="relative py-12 sm:py-16 lg:pt-20 xl:pb-24">
         <div className="relative px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
           <div className="max-w-6xl mx-auto text-center">
             <h1 className="mt-5 text-4xl font-bold leading-tight text-gray-900 sm:text-5xl sm:leading-tight lg:text-6xl lg:leading-tight font-pj">
@@ -12,9 +12,8 @@ const HeroSection = () => {
               for users while driving sales.
             </p>
 
-            <div className="relative inline-flex mt-10 md:mb-20 group">
+            <div className="relative inline-flex mt-10 md:mb-0 group">
               <div className="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt"></div>
-
               <a
                 href="#"
                 title=""
